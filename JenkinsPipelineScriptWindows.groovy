59da06e5c0130a21fe1492753fa0f376ae6409a6
tapefive <tape_five@hotmail.com>
2025-01-23 15:26:50 +0000

    add Jenkins script

---
+++ JenkinsPipelineScriptWindows.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git branch: 'master', url: 'https://github.com/tapefive/Postman-API-Automation'
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                // Install Newman locally
+                bat 'npm install newman'
+                
+                // Confirm Newman installation
+                bat 'node node_modules\\newman\\bin\\newman.js -v'
+            }
+        }
+        stage('Run Postman Collection') {
+            steps {
+                // Run Newman with proper paths and escaping
+                bat """
+                node node_modules\\newman\\bin\\newman.js run C:\\Users\\{USERNAME}\\Desktop\\Postman\\APIAutomation\\GoREST.postman_collection.json ^
+                    --environment C:\\Users\\{USERNAME}\\Desktop\\Postman\\APIAutomation\\MyEnvironment.postman_environment.json ^
+                    --env-var ^"ACCESSTOKEN=${ACCESSTOKEN}^" ^
+                    --reporters cli
+                """
+            }
+        }
+    }
+}
